640d825159c9227683a4ca65f4f578e2764ae47e
VedantVK7 <vedantkannurkar@gmail.com>
2024-11-08 00:59:39 +0530

    done

--- demo.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Running the 'sensors' command and capturing its output
-result = subprocess.run(['sensors'], capture_output=True, text=True)
-
-# Access the output
-output = result.stdout
-print(output)
